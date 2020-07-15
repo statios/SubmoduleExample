144e8e2fe388a48c83946be1c77eb7548499f084
Stat.So <stat.so@insiar.com>
2020-07-15 17:27:51 +0900

    no message

---
+++ SubmoduleExample/SubmoduleExample/CustomClass.swift
@@ -0,0 +1,14 @@
+//
+//  CustomClass.swift
+//  SubmoduleExample
+//
+//  Created by Stat.So on 2020/07/15.
+//  Copyright © 2020 Stat.So. All rights reserved.
+//
+
+import UIKit
+
+public class Navigator {
+  public var name = "stat"
+  public init() { }
+}
