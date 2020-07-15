fa3af0362a6df3325d8c27c4077df77292f7b35b
Stat.So <stat.so@insiar.com>
2020-07-15 17:36:35 +0900

    no message

--- SubmoduleExample/SubmoduleExample/CustomClass.swift
+++ SubmoduleExample/SubmoduleExample/CustomClass.swift
@@ -6,14 +6,7 @@
 //  Copyright © 2020 Stat.So. All rights reserved.
 //
 
-<<<<<<< HEAD:SubmoduleExample/SubmoduleExample/Navigator.swift
 public class CustomClass {
-  public var somestr = "BUZZ"
-=======
-import UIKit
-
-public class Navigator {
   public var name = "stat"
->>>>>>> 144e8e2fe388a48c83946be1c77eb7548499f084:SubmoduleExample/SubmoduleExample/CustomClass.swift
   public init() { }
 }
