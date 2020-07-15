484f7310f8e36a78d301c90f0e21b2cca4e2c028
Stat.So <stat.so@insiar.com>
2020-07-15 16:36:56 +0900

    no message

---
+++ SubmoduleExample/SubmoduleExample/SubmoduleExample.h
@@ -0,0 +1,19 @@
+//
+//  SubmoduleExample.h
+//  SubmoduleExample
+//
+//  Created by Stat.So on 2020/07/15.
+//  Copyright © 2020 Stat.So. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubmoduleExample.
+FOUNDATION_EXPORT double SubmoduleExampleVersionNumber;
+
+//! Project version string for SubmoduleExample.
+FOUNDATION_EXPORT const unsigned char SubmoduleExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubmoduleExample/PublicHeader.h>
+
+
